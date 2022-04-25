a11fffbc9223a857b2eb392463a5a4e2f544d921
redhatho <redha_theo@hotmail.fr>
2022-04-25 11:36:01 +0200

    correction mineure

--- front/js/script.js
+++ front/js/script.js
@@ -4,9 +4,10 @@ fetch("http://localhost:3000/api/products")
   // Répartition des données de l'API dans le DOM
   .then(function (productFromApi) {
     const products = productFromApi;
-    for (let product in products) {// Insertion des éléments contenu dans l'API dans l'index
+    // Insertion des éléments contenu dans l'API dans l'index
+    for (let product in products) {
       document.getElementById('items').innerHTML += `<a href="product.html?id=${productFromApi[product]._id}">
-        
+
         <article>
           <img src="${productFromApi[product].imageUrl}" alt="${productFromApi[product].altTxt}">
           <h3 class="name">${productFromApi[product].name}</h3>
